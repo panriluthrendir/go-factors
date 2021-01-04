7163809dda4621de537e48e8d1c28280a093cec2
panriluthrendir <59873034+panriluthrendir@users.noreply.github.com>
2021-01-04 10:56:47 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-    "errors"
     "fmt"
     "net/http"
     "os"
@@ -28,55 +27,4 @@ func main() {
     })
 
     http.ListenAndServe(":"+PORT, nil)
-}
-
-func factorize(num int, primes []int) (map[int]int, error) {
-    result := make(map[int]int)
-
-    for _, p := range primes {
-        if num == 1 {
-            return result, nil
-        }
-        if p*p > num {
-            result[num] = 1
-            return result, nil
-        }
-        i := 0
-        for {
-            if num%p == 0 {
-                i++
-                num = num / p
-            } else {
-                break
-            }
-        }
-        if i > 0 {
-            result[p] = i
-        }
-    }
-    return nil, errors.New("The number is too large to factorize")
-}
-
-func primesUnder(N int) []int {
-    isComposite := make([]bool, 0)
-
-    for i := 0; i < N; i++ {
-        isComposite = append(isComposite, false)
-    }
-
-    for n := 2; n < N/2; n++ {
-        for i := 2; i < N/n; i++ {
-            isComposite[i*n] = true
-        }
-    }
-
-    result := make([]int, 0)
-
-    for n := 2; n < N; n++ {
-        if !isComposite[n] {
-            result = append(result, n)
-        }
-    }
-
-    return result
 }
