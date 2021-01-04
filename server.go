5b5203116e9c0c5861a234ae7311a8a888de0266
panriluthrendir <59873034+panriluthrendir@users.noreply.github.com>
2021-01-04 11:09:57 +0100

    Update server.go

--- server.go
+++ server.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
     PORT := os.Getenv("PORT")
-    PRIMES := primesUnder(10e5)
+    PRIMES := primesUnder(10e7)
 
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
         keys, present := r.URL.Query()["num"]
