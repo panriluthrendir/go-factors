885dca43b0140f8396675c150a78d69e47709ae7
panriluthrendir <59873034+panriluthrendir@users.noreply.github.com>
2021-01-04 07:59:37 +0100

    Update main.go

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
+	PORT, _ := os.Getenv("PORT")
+	
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello World!")
 	})
 
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":" + PORT, nil)
 }
