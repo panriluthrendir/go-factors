76eed870bac1a4232f2056de77ae00b0257a7b02
panriluthrendir <59873034+panriluthrendir@users.noreply.github.com>
2021-01-05 08:15:28 +0100

    Update server.go

--- server.go
+++ server.go
@@ -5,26 +5,31 @@ import (
     "net/http"
     "os"
     "strconv"
-    "encoding/json"
 )
+
+func renderTemplate(w http.ResponseWriter, tmpl string, factors map[int]int) {
+    t, _ := template.ParseFiles(tmpl + ".html")
+    t.Execute(w, factors)
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+    if r.Method == http.MethodGet {
+        renderTemplate(w, "form", nil)
+    } else {
+        num, err := strconv.Atoi(r.FormValue("number"))
+        if err != nil {
+            renderTemplate(w, "form", nil)
+        } else {
+            factors, _ := factorize(num, PRIMES)
+            renderTemplate(w, "form", factors)
+        }
+    }
+}
 
 func main() {
     PORT := os.Getenv("PORT")
     PRIMES := primesUnder(10e6)
 
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        keys, present := r.URL.Query()["num"]
-        if present {
-            num, err := strconv.Atoi(keys[0])
-            if err != nil {
-                fmt.Fprint(w, err)
-            } else {
-                factors, _ := factorize(num, PRIMES)
-                factorsJson, _ := json.Marshal(factors)
-                fmt.Fprint(w, string(factorsJson))
-            }
-        }
-    })
-
+    http.HandleFunc("/", handle)
     http.ListenAndServe(":"+PORT, nil)
 }
